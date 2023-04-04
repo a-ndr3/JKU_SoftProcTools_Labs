28fdd6fa2419d0c055864f88e2a481bcf892dbe2
Andrei Blokhin <andrei_blokhin@outlook.com>
2023-04-05 01:12:58 +0200

    Assignment_1 Code Review Process
    Added: Code Review Process plugin

---
+++ publish_1/scripts/processElementData.js
@@ -0,0 +1,99 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.elementUrls["_We8ucNFmEe2T9Kw2TWBJNg"] = ["Specification Requirements","CodeReviewPlugin/deliveryprocesses/specification_requirements_C873E94F.html"];
+contentPage.processPage.elementUrls["_ziaIhNFkEe2T9Kw2TWBJNg"] = ["Run Code Analysis","CodeReviewPlugin/deliveryprocesses/run_code_analysis_41E56134.html"];
+contentPage.processPage.elementUrls["_ZUgttNFjEe2T9Kw2TWBJNg"] = ["Project Task List","CodeReviewPlugin/deliveryprocesses/project_task_list_4B9F8018.html"];
+contentPage.processPage.elementUrls["_5oiueNFmEe2T9Kw2TWBJNg"] = ["Specification for reviews","CodeReviewPlugin/deliveryprocesses/specification_for_reviews_47C883A8.html"];
+contentPage.processPage.elementUrls["_5oiudNFmEe2T9Kw2TWBJNg"] = ["Project Status Reports","CodeReviewPlugin/deliveryprocesses/project_status_reports_8336A189.html"];
+contentPage.processPage.elementUrls["_5oiuYdFmEe2T9Kw2TWBJNg"] = ["Track Progress of Code Review","CodeReviewPlugin/deliveryprocesses/track_progress_of_code_review_FF8AFE4A.html"];
+contentPage.processPage.elementUrls["_ziaIj9FkEe2T9Kw2TWBJNg"] = ["Source Code","CodeReviewPlugin/deliveryprocesses/source_code_2D19EF5D.html"];
+contentPage.processPage.elementUrls["_ziaIp9FkEe2T9Kw2TWBJNg"] = ["Specification for analysts","CodeReviewPlugin/deliveryprocesses/specification_for_analysts_C8853C17.html"];
+contentPage.processPage.elementUrls["_ziaIndFkEe2T9Kw2TWBJNg"] = ["Following Coding Standards","CodeReviewPlugin/deliveryprocesses/following_coding_standards_CBE9C204.html"];
+contentPage.processPage.elementUrls["_Wezkj9FmEe2T9Kw2TWBJNg"] = ["Review","CodeReviewPlugin/deliveryprocesses/review_39935507.html"];
+contentPage.processPage.elementUrls["_WezkndFmEe2T9Kw2TWBJNg"] = ["Source Code","CodeReviewPlugin/deliveryprocesses/source_code_D86327AE.html"];
+contentPage.processPage.elementUrls["_Wezkl9FmEe2T9Kw2TWBJNg"] = ["Updated Code","CodeReviewPlugin/deliveryprocesses/updated_code_C2B71945.html"];
+contentPage.processPage.elementUrls["_ziaIo9FkEe2T9Kw2TWBJNg"] = ["Project Review Plan","CodeReviewPlugin/deliveryprocesses/project_review_plan_3F359F8.html"];
+contentPage.processPage.elementUrls["_ziaIh9FkEe2T9Kw2TWBJNg"] = ["Quality Analyst","CodeReviewPlugin/deliveryprocesses/quality_analyst_A3F62B1F.html"];
+contentPage.processPage.elementUrls["_ziaIkdFkEe2T9Kw2TWBJNg"] = ["Test report","CodeReviewPlugin/deliveryprocesses/test_report_7E341BA7.html"];
+contentPage.processPage.elementUrls["_5oiuYNFmEe2T9Kw2TWBJNg"] = ["Monitor Effectivness of Code Review","CodeReviewPlugin/deliveryprocesses/monitor_effectivness_of_code_review_10F1EA34.html"];
+contentPage.processPage.elementUrls["_5oiuaNFmEe2T9Kw2TWBJNg"] = ["Review Documents","CodeReviewPlugin/deliveryprocesses/project_review_documents_3580FB2C.html"];
+contentPage.processPage.elementUrls["_WezkgNFmEe2T9Kw2TWBJNg"] = ["Assist Developer in addressing issues","CodeReviewPlugin/deliveryprocesses/assist_developer_in_addressing_issues_89CCE4BF.html"];
+contentPage.processPage.elementUrls["_ZUW8xNFjEe2T9Kw2TWBJNg"] = ["Specification for reviews","CodeReviewPlugin/deliveryprocesses/specification_for_reviews_AFDAB968.html"];
+contentPage.processPage.elementUrls["_WezkldFmEe2T9Kw2TWBJNg"] = ["Tests","CodeReviewPlugin/deliveryprocesses/tests_4F3F6370.html"];
+contentPage.processPage.elementUrls["_WezkjdFmEe2T9Kw2TWBJNg"] = ["Project Manager","CodeReviewPlugin/deliveryprocesses/project_manager_C61B9F32.html"];
+contentPage.processPage.elementUrls["_ziaImdFkEe2T9Kw2TWBJNg"] = ["Specification Requirements","CodeReviewPlugin/deliveryprocesses/specification_requirements_757DFE5.html"];
+contentPage.processPage.elementUrls["_5oiuYtFmEe2T9Kw2TWBJNg"] = ["Project Manager","CodeReviewPlugin/deliveryprocesses/project_manager_A28DE5A.html"];
+contentPage.processPage.elementUrls["_ZUW8wtFjEe2T9Kw2TWBJNg"] = ["Source Code","CodeReviewPlugin/deliveryprocesses/source_code_E47FCB6F.html"];
+contentPage.processPage.elementUrls["_qP6CYdFkEe2T9Kw2TWBJNg"] = ["Review","CodeReviewPlugin/deliveryprocesses/Review_FC215FCE.html"];
+contentPage.processPage.elementUrls["_Wezkg9FmEe2T9Kw2TWBJNg"] = ["Quality Analyst","CodeReviewPlugin/deliveryprocesses/quality_analyst_EBDDAEAA.html"];
+contentPage.processPage.elementUrls["_12u7kdFkEe2T9Kw2TWBJNg"] = ["Specification for tests","CodeReviewPlugin/deliveryprocesses/specification_for_tests_FDF0573.html"];
+contentPage.processPage.elementUrls["_ziaIhdFkEe2T9Kw2TWBJNg"] = ["Tester","CodeReviewPlugin/deliveryprocesses/tester_307E754A.html"];
+contentPage.processPage.elementUrls["_ZUW8vNFjEe2T9Kw2TWBJNg"] = ["Tester","CodeReviewPlugin/deliveryprocesses/tester_26B6F52A.html"];
+contentPage.processPage.elementUrls["_ziaIldFkEe2T9Kw2TWBJNg"] = ["Improved Code Quality","CodeReviewPlugin/deliveryprocesses/improved_code_quality_42C5FDC6.html"];
+contentPage.processPage.elementUrls["_5oiudtFmEe2T9Kw2TWBJNg"] = ["Analysis Report","CodeReviewPlugin/deliveryprocesses/analysis_report_7C6D95AF.html"];
+contentPage.processPage.elementUrls["_Wezkh9FmEe2T9Kw2TWBJNg"] = ["Tester","CodeReviewPlugin/deliveryprocesses/tester_B06F90C9.html"];
+contentPage.processPage.elementUrls["_WezkmdFmEe2T9Kw2TWBJNg"] = ["Increased Team Collaboration","CodeReviewPlugin/deliveryprocesses/increased_team_collaboration_13D1458F.html"];
+contentPage.processPage.elementUrls["_12u7k9FkEe2T9Kw2TWBJNg"] = ["Tests","CodeReviewPlugin/deliveryprocesses/tests_8356BB48.html"];
+contentPage.processPage.elementUrls["_5oiubtFmEe2T9Kw2TWBJNg"] = ["Review","CodeReviewPlugin/deliveryprocesses/review_F349D171.html"];
+contentPage.processPage.elementUrls["_Wezki9FmEe2T9Kw2TWBJNg"] = ["Software Developer","CodeReviewPlugin/deliveryprocesses/software_developer_750172E8.html"];
+contentPage.processPage.elementUrls["_ziaIk9FkEe2T9Kw2TWBJNg"] = ["Analysis Report","CodeReviewPlugin/deliveryprocesses/analysis_report_F1ABD17C.html"];
+contentPage.processPage.elementUrls["_WezkodFmEe2T9Kw2TWBJNg"] = ["Review Documents","CodeReviewPlugin/deliveryprocesses/project_review_documents_9CF509CD.html"];
+contentPage.processPage.elementUrls["_ZUW8tNFjEe2T9Kw2TWBJNg"] = ["Software Developer","CodeReviewPlugin/deliveryprocesses/software_developer_9D9330EC.html"];
+contentPage.processPage.elementUrls["_09CBgdFmEe2T9Kw2TWBJNg"] = ["Monitoring_and_Reporting","CodeReviewPlugin/deliveryprocesses/Monitoring_and_Reporting_1BADA21E.html"];
+contentPage.processPage.elementUrls["_Wezkm9FmEe2T9Kw2TWBJNg"] = ["Improved Code Quality","CodeReviewPlugin/deliveryprocesses/improved_code_quality_8748FB64.html"];
+contentPage.processPage.elementUrls["_Wezkk9FmEe2T9Kw2TWBJNg"] = ["Test report","CodeReviewPlugin/deliveryprocesses/test_report_FE253726.html"];
+contentPage.processPage.elementUrls["_ziaIidFkEe2T9Kw2TWBJNg"] = ["Senior Reviewer","CodeReviewPlugin/deliveryprocesses/senior_reviewer_F5105769.html"];
+contentPage.processPage.elementUrls["_WezkhdFmEe2T9Kw2TWBJNg"] = ["Reviewer","CodeReviewPlugin/deliveryprocesses/reviewer_3CF7DAF4.html"];
+contentPage.processPage.elementUrls["_ZUW8sdFjEe2T9Kw2TWBJNg"] = ["Plan Review Process","CodeReviewPlugin/deliveryprocesses/plan_review_process_C79A62E3.html"];
+contentPage.processPage.elementUrls["_ziaIg9FkEe2T9Kw2TWBJNg"] = ["Rewiew of code","CodeReviewPlugin/deliveryprocesses/rewiew_of_code_DF644900.html"];
+contentPage.processPage.elementUrls["_ZUW8wNFjEe2T9Kw2TWBJNg"] = ["Specification for Developer","CodeReviewPlugin/deliveryprocesses/specification_for_developer_EB48D749.html"];
+contentPage.processPage.elementUrls["_ziaIpdFkEe2T9Kw2TWBJNg"] = ["Review","CodeReviewPlugin/deliveryprocesses/review_550D8642.html"];
+contentPage.processPage.elementUrls["_WezkkdFmEe2T9Kw2TWBJNg"] = ["Analysis Report","CodeReviewPlugin/deliveryprocesses/analysis_report_8AAD8151.html"];
+contentPage.processPage.elementUrls["_12u7kNFkEe2T9Kw2TWBJNg"] = ["Write Tests","CodeReviewPlugin/deliveryprocesses/write_tests_2145F15D.html"];
+contentPage.processPage.elementUrls["_ZUW8ttFjEe2T9Kw2TWBJNg"] = ["Senior Reviewer","CodeReviewPlugin/deliveryprocesses/senior_reviewer_96CA2512.html"];
+contentPage.processPage.elementUrls["_ZUgtsNFjEe2T9Kw2TWBJNg"] = ["Code Review Plan","CodeReviewPlugin/deliveryprocesses/code_review_plan_870D9DF9.html"];
+contentPage.processPage.elementUrls["_5oiuZtFmEe2T9Kw2TWBJNg"] = ["Code Review Plan","CodeReviewPlugin/deliveryprocesses/code_review_plan_CEBAC079.html"];
+contentPage.processPage.elementUrls["_ZUW8stFjEe2T9Kw2TWBJNg"] = ["Set Code for Review","CodeReviewPlugin/deliveryprocesses/set_code_for_review_D23842F3.html"];
+contentPage.processPage.elementUrls["_Souq0dFmEe2T9Kw2TWBJNg"] = ["Feedback_and_Updates","CodeReviewPlugin/deliveryprocesses/Feedback_and_Updates_ECC62B65.html"];
+contentPage.processPage.elementUrls["_5oiuatFmEe2T9Kw2TWBJNg"] = ["Project Review Plan","CodeReviewPlugin/deliveryprocesses/project_review_plan_2EB7EF52.html"];
+contentPage.processPage.elementUrls["_5oiuetFmEe2T9Kw2TWBJNg"] = ["Project Task List","CodeReviewPlugin/deliveryprocesses/project_task_list_40FF77CE.html"];
+contentPage.processPage.elementUrls["_5oiuZNFmEe2T9Kw2TWBJNg"] = ["Senior Reviewer","CodeReviewPlugin/deliveryprocesses/senior_reviewer_D583CC53.html"];
+contentPage.processPage.elementUrls["_ziaIgdFkEe2T9Kw2TWBJNg"] = ["Read Code and Documentation","CodeReviewPlugin/deliveryprocesses/read_code_and_documentation_6BEC932B.html"];
+contentPage.processPage.elementUrls["_ziaIm9FkEe2T9Kw2TWBJNg"] = ["Specification for Developer","CodeReviewPlugin/deliveryprocesses/specification_for_developer_7ACF95BA.html"];
+contentPage.processPage.elementUrls["_tl9No9FjEe2T9Kw2TWBJNg"] = ["Following Coding Standards","CodeReviewPlugin/deliveryprocesses/following_coding_standards_7BCADA6D.html"];
+contentPage.processPage.elementUrls["_ZUgtstFjEe2T9Kw2TWBJNg"] = ["Project Review Plan","CodeReviewPlugin/deliveryprocesses/project_review_plan_8044921F.html"];
+contentPage.processPage.elementUrls["_WezkidFmEe2T9Kw2TWBJNg"] = ["Senior Reviewer","CodeReviewPlugin/deliveryprocesses/senior_reviewer_189BD13.html"];
+contentPage.processPage.elementUrls["_Wezko9FmEe2T9Kw2TWBJNg"] = ["Specification for reviews","CodeReviewPlugin/deliveryprocesses/specification_for_reviews_106CBFA2.html"];
+contentPage.processPage.elementUrls["_tl9NodFjEe2T9Kw2TWBJNg"] = ["Specification Requirements","CodeReviewPlugin/deliveryprocesses/specification_requirements_8532498.html"];
+contentPage.processPage.elementUrls["_Vj7_EdFjEe2T9Kw2TWBJNg"] = ["Preparation","CodeReviewPlugin/deliveryprocesses/Preparation_C49312F1.html"];
+contentPage.processPage.elementUrls["_ZUgtttFjEe2T9Kw2TWBJNg"] = ["Increased Team Collaboration","CodeReviewPlugin/deliveryprocesses/increased_team_collaboration_44D6743E.html"];
+contentPage.processPage.elementUrls["_ziaIgtFkEe2T9Kw2TWBJNg"] = ["Review of Requirements","CodeReviewPlugin/deliveryprocesses/review_of_requirements_768A733B.html"];
+contentPage.processPage.elementUrls["_WezkgtFmEe2T9Kw2TWBJNg"] = ["Write Report on Code Review","CodeReviewPlugin/deliveryprocesses/write_report_on_code_review_8303D8E5.html"];
+contentPage.processPage.elementUrls["_ziaIn9FkEe2T9Kw2TWBJNg"] = ["Specification for reviews","CodeReviewPlugin/deliveryprocesses/specification_for_reviews_3F6177D9.html"];
+contentPage.processPage.elementUrls["_ziaIgNFkEe2T9Kw2TWBJNg"] = ["Check Test Results","CodeReviewPlugin/deliveryprocesses/check_test_results_7D537F15.html"];
+contentPage.processPage.elementUrls["_5oiucNFmEe2T9Kw2TWBJNg"] = ["Improved Code Quality","CodeReviewPlugin/deliveryprocesses/improved_code_quality_BEA4BF6A.html"];
+contentPage.processPage.elementUrls["_5oiuctFmEe2T9Kw2TWBJNg"] = ["Increased Team Collaboration","CodeReviewPlugin/deliveryprocesses/increased_team_collaboration_B7DBB390.html"];
+contentPage.processPage.elementUrls["_ZUW8vtFjEe2T9Kw2TWBJNg"] = ["Quality Analyst","CodeReviewPlugin/deliveryprocesses/quality_analyst_1FEDE950.html"];
+contentPage.processPage.elementUrls["_WezkgdFmEe2T9Kw2TWBJNg"] = ["Update Code","CodeReviewPlugin/deliveryprocesses/update_code_7865F8D5.html"];
+contentPage.processPage.elementUrls["_ZUW8sNFjEe2T9Kw2TWBJNg"] = ["Develop Code","CodeReviewPlugin/deliveryprocesses/develop_code_D9014ECD.html"];
+contentPage.processPage.elementUrls["_ziaIi9FkEe2T9Kw2TWBJNg"] = ["Reviewer","CodeReviewPlugin/deliveryprocesses/reviewer_68880D3E.html"];
+contentPage.processPage.elementUrls["_ziaIodFkEe2T9Kw2TWBJNg"] = ["Code Review Plan","CodeReviewPlugin/deliveryprocesses/code_review_plan_907BA423.html"];
+contentPage.processPage.elementUrls["_ZUW8utFjEe2T9Kw2TWBJNg"] = ["Reviewer","CodeReviewPlugin/deliveryprocesses/reviewer_5B5C0731.html"];
+contentPage.processPage.elementUrls["_tl9NoNFjEe2T9Kw2TWBJNg"] = ["Review of Requirements","CodeReviewPlugin/deliveryprocesses/review_of_requirements_19BA1082.html"];
+contentPage.processPage.elementUrls["_ziaIl9FkEe2T9Kw2TWBJNg"] = ["Review Documents","CodeReviewPlugin/deliveryprocesses/project_review_documents_B63DB39B.html"];
+contentPage.processPage.elementUrls["_Wezkn9FmEe2T9Kw2TWBJNg"] = ["Code Review Plan","CodeReviewPlugin/deliveryprocesses/code_review_plan_4BDADD83.html"];
+contentPage.processPage.elementUrls["_ZUW8uNFjEe2T9Kw2TWBJNg"] = ["Project Manager","CodeReviewPlugin/deliveryprocesses/project_manager_6225130B.html"];
+contentPage.processPage.elementUrls["_ziaIjdFkEe2T9Kw2TWBJNg"] = ["Project Manager","CodeReviewPlugin/deliveryprocesses/project_manager_B9A23988.html"];
+contentPage.processPage.elementUrls["_5oiubNFmEe2T9Kw2TWBJNg"] = ["Review Report","CodeReviewPlugin/deliveryprocesses/review_report_FA12DD4B.html"];
+contentPage.processPage.elementUrls["_We8uctFmEe2T9Kw2TWBJNg"] = ["Review Report","CodeReviewPlugin/deliveryprocesses/review_report_C1AADD75.html"];
